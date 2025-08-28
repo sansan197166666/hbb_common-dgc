bdf8ac23b055bb48d2545de111f6fb7372a7acd6
sansan197166666 <sansan197166666@gmail.com>
2025-08-29 02:32:15 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,7 +98,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["54.82.84.32"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["154.82.84.32"];
 pub const RS_PUB_KEY: &str = "YYy4QBgCfGDLI79kVRKLdZIzyCvL1KMb4+u5lZ5bmF0=";
 pub const RENDEZVOUS_PORT: i32 = 56136;
 pub const RELAY_PORT: i32 = 56137;
